a9c7ab6b950c9cbac09eac79e835c4b7f441bdcc
Zwea Htet <70414756+zweahtet@users.noreply.github.com>
2021-12-04 12:33:23 -0800

    add nojekll file to bypass Jekyll on Giithub pages

---
+++ out/_next/static/webpack/webpack.139ff80ea5e0e8a58060.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "8cae49b7e418819c02a7"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay4xMzlmZjgwZWE1ZTBlOGE1ODA2MC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjhjYWU0OWI3ZTQxODgxOWMwMmE3XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
